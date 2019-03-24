573e0f782c10693869bcb4598888e7083ca3b4a5
Evgenii <aksyline@gmail.com>
2019-03-24 20:11:08 +0300

    add https server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,18 @@
 import express from 'express'
 import Loadable from 'react-loadable'
+import fs from 'fs'
+import http from 'http'
+import https from 'https'
+
+let PRIVATE_KEY = null
+let CERTIFICATE = null
+let CREDENTIALS = {}
+if (process.env.HTTPS) {
+  PRIVATE_KEY = fs.readFileSync(process.env.PK_PATH, 'utf8')
+  CERTIFICATE = fs.readFileSync(Process.env.CERT_PATH, 'utf8')
+  CREDENTIALS.key = PRIVATE_KEY
+  CREDENTIALS.cert = CERTIFICATE
+}
 
 import indexController from './controller'
 
@@ -12,10 +25,13 @@ app.use(indexController)
 
 // start the app
 Loadable.preloadAll().then(() => {
-  app.listen(PORT, (error) => {
-    if (error) {
-      return console.log('something bad happened', error)
-    }
-    console.log("listening on " + PORT + "...")
-  })
+  let httpServer = http.createServer(app)
+  let httpsServer = null
+  if (process.env.HTTPS) {
+    httpsServer = https.createServer(app, CREDENTIALS)
+  }
+  if (process.env.HTTPS) {
+    httpsServer.listen(443, () => console.log(`HTTPS server has started on ${443} port`))
+  }
+  httpServer.listen(PORT, () => console.log(`HTTP server has started at port ${PORT}`))
 })
